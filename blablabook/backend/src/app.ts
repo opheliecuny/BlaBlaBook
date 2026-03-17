5f1d85cca45d9919cbb133a855b4a7d794ba7cb1
Ophélie Cuny <ophelie.cuny@oclock.school>
2026-03-17 14:02:33 +0100

    feat(api): error middleware imported in app.ts

--- blablabook/backend/src/app.ts
+++ blablabook/backend/src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express"; 
 import { config } from "../config";
 import { router as apiRouter } from "./routes/index.router.ts";
+import { errorHandler } from "./middlewares/errorHandler";
 
 
 export const app = express(); 
@@ -11,3 +12,4 @@ app.use(cors({ origin: config.allowedOrigins, credentials: true }));
 app.use(express.json()); 
 
 app.use(apiRouter);
+app.use(errorHandler);
